3f3c5471e2414d43e81b379d0954690e8b9c17f4
anievas <arturo98nievas@gmail.com>
2024-06-10 20:32:41 -0300

    Funcion validar campos

--- inmobiliaria/src/utils/validarCampos.js
+++ inmobiliaria/src/utils/validarCampos.js
@@ -28,31 +28,42 @@ $validacion = [
 function validarCampos(datos,validacion){
     let errores=[];
 
-    for(let reglas in validacion){
-        let existe=((datos[reglas]!==null) && (datos[reglas]!==''));
-        for(let regla in validacion[reglas]){
+    for(let campo in validacion){
+        let existe=((datos[campo]!==null) && (datos[campo]!==''));
+        for(let regla in validacion[campo]){
             switch(regla){
                 case 'requerido':
                     if(!existe){
-                        errores.push(`El campo ${reglas} es requerido`);
+                        errores[campo] = campo + " es requerido.";
                     }
                     break;
                 case 'bool':
-                    console.log('bool');
+                    if (existe && typeof datos[campo] !== 'boolean') {
+                        errores[campo] = campo + " debe ser un valor booleano.";
+                    }
                     break;
                 case 'int':
-                    console.log('int');
+                    if (existe && !Number.isInteger(Number(datos[campo]))) {
+                        errores[campo] = campo + " debe ser un entero.";
+                    }
                     break;
                 case 'fecha':
-                    console.log('fecha');
+                    if (existe && isNaN(Date.parse(datos[campo]))) {
+                        errores[campo] = campo + " debe ser una fecha válida.";
+                    }
                     break;
                 case 'double':
-                    console.log('double');
+                    if (existe && isNaN(parseFloat(datos[campo]))) {
+                        errores[campo] = campo + " debe ser un número.";
+                    }
+                    break;
+                case 'longitud':
+                    if (existe && datos[campo].length > validacion[campo][regla]) {
+                        errores[campo] = campo + " debe tener un máximo de " + validacion[campo][regla] + " caracteres.";
+                    }
                     break;
                 default:
-                    if(existe && datos[reglas].length>validacion[reglas][regla]){
-                        errores.push(`El campo ${reglas} debe tener un maximo de ${validacion[reglas][regla]} caracteres.`);
-                    }
+                    break;
             }
         }
     }
