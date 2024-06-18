873550e0b4593055c811b2f260c14ceb61bb7782
MaximilianoFalasca <104387492+MaximilianoFalasca@users.noreply.github.com>
2024-06-18 09:59:53 -0300

    Update TipoPropiedadPage.jsx

--- inmobiliaria/src/pages/tipoPropiedad/TipoPropiedadPage.jsx
+++ inmobiliaria/src/pages/tipoPropiedad/TipoPropiedadPage.jsx
@@ -19,8 +19,9 @@ function TipoPropiedadPage() {
     setType(type);
   }
 
-  function handleClickDelete({ elemento }) {
-    conexionServer(`tipos_propiedad/${elemento.id}`, setData, setState, "DELETE");
+  function handleClickDelete( id ) {
+    conexionServer(`tipos_propiedad/${id}`, setData, setState, "DELETE");
+    
   }
 
   const childrenItem = (propiedad) => (
@@ -28,7 +29,60 @@ function TipoPropiedadPage() {
       <p className='title-li'>{propiedad.nombre}</p>
       <div className='buttons'>
         <ButtonComponent type="edit" handleClick={() => handleClick({ type: 'edit' })} />
-        <ButtonComponent type="delete" handleClick={() => handleClickDelete({ elemento: propiedad })} />
+        <ButtonComponent type="delete" handleClick={() => handleClickDelete( propiedad.id )} />
+      </div>
+    </ListItemComponent>
+  );
+
+  return (
+    <>
+      <HeaderComponent />
+      <main>
+        {state === "ERROR" ? (
+          // manejo del error.
+          <p>Error:</p>
+        ) : (
+          <UlComponent data={data} state={state} childrenItem={childrenItem} />
+        )}
+      </main>
+      <FooterComponent />
+    </>
+  );
+}
+
+export default TipoPropiedadPage;
+import React, { useEffect, useState } from 'react';
+import HeaderComponent from '../../components/HeaderComponent';
+import FooterComponent from '../../components/FooterComponent';
+import conexionServer from '../../utils/conexionServer';
+import ListItemComponent from '../../components/ListitemComponent';
+import ButtonComponent from '../../components/ButtonComponent';
+import UlComponent from '../../components/UlComponent';
+
+function TipoPropiedadPage() {
+  const [data, setData] = useState([]);
+  const [type, setType] = useState([]);
+  const [state, setState] = useState("LOADING");
+
+  useEffect(() => {
+    conexionServer("tipos_propiedad", setData, setState);
+  },[]);
+
+  function handleClick({ type }) {
+    setType(type);
+  }
+
+  function handleClickDelete( id ) {
+    conexionServer(`tipos_propiedad/${id}`, setData, setState, "DELETE");
+    
+  }
+
+  const childrenItem = (propiedad) => (
+    <ListItemComponent key={propiedad.id} propiedad={propiedad} url={`/tipos_propiedad/${type === "edit" ? `${type}/${propiedad.id}` : ``}`} tipo={type}>
+      <p className='title-li'>{propiedad.nombre}</p>
+      <div className='buttons'>
+        <ButtonComponent type="edit" handleClick={() => handleClick({ type: 'edit' })} />
+        <ButtonComponent type="delete" handleClick={() => handleClickDelete( propiedad.id )} />
       </div>
     </ListItemComponent>
   );
